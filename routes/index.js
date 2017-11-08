9d119129a753e387e6c800d32a976d878df5bf8b
Amir Patel <amirpatel89@gmail.com>
2017-11-08 14:50:34 -0500

    added EJS files and updated index.js route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,41 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/blog', function(req, res, next) {
+  res.render('blog',{});
+});
+
+router.get('/donate', function(req, res, next) {
+  res.render('donate',{});
+});
+
+router.get('/map', function(req, res, next) {
+  res.render('map',{});
+});
+
+router.get('/resources', function(req, res, next) {
+  res.render('resources',{});
+});
+
+router.get('/user-signup-page', function(req, res, next) {
+  res.render('user-signup-page',{});
+});
+
+router.get('/user-home', function(req, res, next) {
+  res.render('user-home',{});
+});
+
+router.get('/volunteer-signup-form', function(req, res, next) {
+  res.render('volunteer-signup-form',{});
+});
+
+router.get('/volunteer-form', function(req, res, next) {
+  res.render('volunteer-form',{});
+});
+
+router.get('/youth-login-form', function(req, res, next) {
+  res.render('youth-login-form',{});
+});
+
+
 module.exports = router;
