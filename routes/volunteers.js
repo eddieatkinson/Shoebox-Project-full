cf1eab66530c4c64835463adb64f2ee7b5bdbc36
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-11-08 15:23:29 -0500

    route set to volunteer login page

--- routes/volunteers.js
+++ routes/volunteers.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET volunteer listing. */
 router.get('/', function(req, res, next) {
-  res.send('volunteer page');
+  res.render('volunteer-form', {});
 });
 
 module.exports = router;
