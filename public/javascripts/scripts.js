e09dd2f6072823e689869b149713a6774193fb5b
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-11-08 14:17:31 -0500

    scripts.js added

---
+++ public/javascripts/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(()=>{
+	localStorage.setItem('user-password', 'myPassword');
+	console.log(typeof(localStorage.getItem('user-password')));
+	localStorage.setItem('vol-password', 'myPassword');
+
+	$('#myModal').on('shown.bs.modal', function () {
+		$('#myInput').focus()
+	});
+	console.log("Connected!");
+	$('.family-login').click(()=>{
+		window.location.href='youth-login-form.html';
+	});
+	$('.volunteer-login').click(()=>{
+		window.location.href='volunteer-login-form.html';
+	});
+	$('.link-to-map').click(()=>{
+		event.preventDefault();
+		window.open("map.html", "_blank", "toolbar=yes,scrollbar=yes,resizeable=yes,top=55,left=75,width=700,height=700");
+	});
+	$(function() {
+		$('#ChangeToggle').click(function() {
+			$('#navbar-hamburger').toggleClass('hidden');
+			$('#navbar-close').toggleClass('hidden');  
+		});
+	});
+
+	// Scrollspy attempt
+	// $('body').scrollspy({target: ".navbar", offset: 50});
+
+	// $('#bs-example-navbar-collapse-1 a').on('click', function(event) {
+	// 	console.log("Didoen!")
+	// 	if(this.hash !== ""){
+	// 		event.preventDefault();
+
+	// 		var hash = this.hash;
+
+	// 		$('html, body').animate({
+	// 			scrollTop: $(hash).offset().top
+	// 		}, 800, function(){
+	// 			window.location.hash = hash;
+	// 		});
+	// 	}
+	// });
+	
+});
