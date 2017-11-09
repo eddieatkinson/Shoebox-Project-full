8dfb5cfc8d72674f17b20a5d91cf44fadc7af238
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-11-09 11:23:46 -0500

    session storage for user login, password authentication for users

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,31 @@ router.get('/home', (req, res, next)=>{
 	res.render('user-home', {});
 });
 
+router.post('/loginProcess', (req, res, next)=>{
+	var email = req.body.email;
+	var password = req.body.password;
+	var selectQuery = `SELECT * FROM users WHERE email = ?;`;
+	connection.query(selectQuery, [email], (error, results)=>{
+		if(error){
+			throw error;
+		}
+		if(results.length == 0){
+			res.redirect('/users/?msg=notRegistered');
+		}else{
+			var passwordsMatch = bcrypt.compareSync(password, results[0].password);
+			if(passwordsMatch){
+				var row = results[0];
+				req.session.name = row.name;
+				req.session.uid = row.id;
+				req.session.email = row.email;
+				console.log(req.session.name);
+
+				res.redirect('/?msg=loggedIn');
+			}else{
+				res.redirect('/users/login?msg=badPass');
+			}
+		}
+	});
+});
+
 module.exports = router;
