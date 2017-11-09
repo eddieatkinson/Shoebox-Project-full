473308fa2673ed08394b8636b51b8c48e0a90728
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-11-09 12:51:53 -0500

    Consolidated scripts files

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -19,7 +19,7 @@ $(document).ready(()=>{
 			$('#navbar-close').toggleClass('hidden');  
 		});
 	});
-	
+
 	// ------------------------------------
 	// -------Volunteer sign-up form-------
 	// ------------------------------------
@@ -45,5 +45,32 @@ $(document).ready(()=>{
 			event.preventDefault();
 			$('.password-error').html("Your passwords do not match.");
 		}
+	});
+
+	// ------------------------------------
+	// ------------Login forms-------------
+	// ------------------------------------
+	var userCount = 0;
+	var volCount = 0;
+	// change color of button when the password input box is focused in
+	$('.youth-sign-up-form').focusin(()=>{
+		userCount++;
+		if(userCount >= 2){
+			$('.submit').removeClass('btn-warning');
+			$('.submit').addClass('btn-success');
+		}
+	});
+	$('.sign-up-form').focusin(()=>{
+		volCount++;
+		if(volCount >= 2){
+			$('.submit').removeClass('btn-warning');
+			$('.submit').addClass('btn-success');
+		}
+	});
+	$(".youth-btn").click(function(){
+		window.location.href = "/"
+	});
+	$(".vol-btn").click(function(){
+		window.location.href = "/volunteers";
 	});
 });
